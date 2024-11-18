e7e6b2bb3ac5562f098ca7b58db3ec5587a68ae2
jl917 <julong1988@naver.com>
2024-11-19 00:05:27 +0900

    fix: update로직 변경

--- src/main/main.ts
+++ src/main/main.ts
@@ -5,47 +5,13 @@ import path from "path";
 import started from "electron-squirrel-startup";
 
 // 자동 업데이트
-import { autoUpdater } from "electron-updater";
+import { updateElectronApp } from "update-electron-app";
 
 function setupAutoUpdater() {
-  autoUpdater.setFeedURL({
-    provider: "github",
+  updateElectronApp({
     repo: "jl917/eApp",
-    owner: "jl917",
-  });
-
-  autoUpdater.checkForUpdatesAndNotify();
-
-  autoUpdater.on("update-available", (info) => {
-    dialog
-      .showMessageBox({
-        type: "info",
-        title: "업데이트 알림",
-        message: "새로운 버전이 있습니다. 다운로드 하시겠습니까?",
-        buttons: ["다운로드", "취소"],
-      })
-      .then((result) => {
-        if (result.response === 0) {
-          // 사용자가 '다운로드' 선택
-          autoUpdater.downloadUpdate();
-        }
-      });
-  });
-
-  autoUpdater.on("update-downloaded", () => {
-    dialog
-      .showMessageBox({
-        type: "info",
-        title: "업데이트 준비 완료",
-        message: "업데이트가 다운로드되었습니다. 지금 설치하시겠습니까?",
-        buttons: ["지금 설치", "나중에"],
-      })
-      .then((result) => {
-        if (result.response === 0) {
-          // 사용자가 '지금 설치' 선택
-          autoUpdater.quitAndInstall();
-        }
-      });
+    updateInterval: "5 minutes",
+    notifyUser: true,
   });
 }
 
