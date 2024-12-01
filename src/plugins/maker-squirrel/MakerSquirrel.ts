acba9c087ca907f3ec9b38aecfb06d88ef99edf8
jl917 <julong1988@naver.com>
2024-12-01 11:56:38 +0900

    fix: 경로 오류 수정

--- src/plugins/maker-squirrel/MakerSquirrel.ts
+++ src/plugins/maker-squirrel/MakerSquirrel.ts
@@ -4,7 +4,7 @@ import { MakerBase, MakerOptions } from "@electron-forge/maker-base";
 import { ForgePlatform } from "@electron-forge/shared-types";
 import { convertVersion, createWindowsInstaller, Options as ElectronWinstallerOptions } from "electron-winstaller";
 import fs from "fs-extra";
-import { getVersion } from "@/utils";
+import { getVersion } from "../../utils";
 
 export type MakerSquirrelConfig = Omit<ElectronWinstallerOptions, "appDirectory" | "outputDirectory">;
 
