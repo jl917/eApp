1900331c9ee4a27998a6d5200cd4206c37990691
jl917 <julong1988@naver.com>
2025-01-05 19:42:51 +0900

    fix: 릴리즈 순서 변경

--- rsbuild.main.config.ts
+++ rsbuild.main.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from '@rsbuild/core';
 import path from 'path';
 import { sentryWebpackPlugin } from '@sentry/webpack-plugin';
 import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
-import { getDefine, getVersion } from './build/utils';
+import { getDefine } from './build/utils';
+import { version } from './package.json';
 
 const mode = process.env.NODE_ENV;
-const version = getVersion();
 const isSourceMap = mode === 'beta' || mode === 'production';
 
 export default defineConfig({
