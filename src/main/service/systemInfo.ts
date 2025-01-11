107173703e463bf764afd36557adfa04616fadaf
jl917 <julong1988@naver.com>
2025-01-11 17:51:38 +0900

    fix: 시스템 정보 업데이트

--- src/main/service/systemInfo.ts
+++ src/main/service/systemInfo.ts
@@ -1,5 +1,33 @@
 import si from 'systeminformation';
 
-export const systemInfo = () => {
-  return si.getStaticData();
+const sortObjectByKeys = (obj: Record<string, any>) => {
+  const sortedKeys = Object.keys(obj).sort(); // 키를 알파벳 순으로 정렬
+  const sortedObj: Record<string, any> = {};
+
+  // 정렬된 키 순서대로 새 객체 생성
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key of sortedKeys) {
+    sortedObj[key] = obj[key];
+  }
+
+  return sortedObj;
+};
+
+export const systemInfo = async () => {
+  const defaultData = await si.getAllData();
+  const audio = await si.audio();
+  const usb = await si.usb();
+  const printer = await si.printer();
+  const networkInterfaces = await si.networkInterfaces();
+  const bluetoothDevices = await si.bluetoothDevices();
+
+  const result = {
+    ...defaultData,
+    audio,
+    usb,
+    printer,
+    networkInterfaces,
+    bluetoothDevices,
+  };
+  return sortObjectByKeys(result);
 };
