a7518e8d27611c52dbf33debaad5eebb412113d8
jl917 <julong1988@naver.com>
2025-01-14 00:27:41 +0900

    fix: 서명 오류 해결

--- src/main/service/systemInfo.ts
+++ src/main/service/systemInfo.ts
@@ -1,7 +1,7 @@
 import si from 'systeminformation';
-// import Store from 'electron-store';
+import Store from 'electron-store';
 
-// const store = new Store();
+const store = new Store();
 
 const sortObjectByKeys = (obj: Record<string, any>) => {
   const sortedKeys = Object.keys(obj).sort(); // 키를 알파벳 순으로 정렬
@@ -17,26 +17,26 @@ const sortObjectByKeys = (obj: Record<string, any>) => {
 };
 
 export const systemInfo = async () => {
-  // if (store.has('systemInfo')) {
-  //   return store.get('systemInfo');
-  // }
+  if (store.has('systemInfo')) {
+    return store.get('systemInfo');
+  }
 
-  // const defaultData = await si.getAllData();
-  // const audio = await si.audio();
-  // const usb = await si.usb();
-  // const printer = await si.printer();
-  // const networkInterfaces = await si.networkInterfaces();
-  // const bluetoothDevices = await si.bluetoothDevices();
+  const defaultData = await si.getAllData();
+  const audio = await si.audio();
+  const usb = await si.usb();
+  const printer = await si.printer();
+  const networkInterfaces = await si.networkInterfaces();
+  const bluetoothDevices = await si.bluetoothDevices();
 
   const result = {
-    // ...defaultData,
-    // audio,
-    // usb,
-    // printer,
-    // networkInterfaces,
-    // bluetoothDevices,
+    ...defaultData,
+    audio,
+    usb,
+    printer,
+    networkInterfaces,
+    bluetoothDevices,
   };
 
-  // store.set('systemInfo', result);
+  store.set('systemInfo', result);
   return sortObjectByKeys(result);
 };
