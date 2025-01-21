cef35a30c8b691cb66db7bb09a8771b11f1123aa
jl917 <julong1988@naver.com>
2025-01-21 23:13:12 +0900

    fix: sync 맞추기

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,8 +14,11 @@ import { RSBUILD_MODE } from '@/common/constant';
 
 const powerService = powerSystem();
 
+// windows처리 필요
 // Set userData path
 app.setPath('userData', path.join(app.getPath('userData'), RSBUILD_MODE));
+app.setPath('exe', path.join(app.getPath('exe'), RSBUILD_MODE));
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit();
