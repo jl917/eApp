37d9fcd3ac7623b25bab67a99cdcb01affb6a89e
jl917 <julong1988@naver.com>
2025-01-25 23:29:37 +0900

    fix: 오류 수정

--- src/main/main.ts
+++ src/main/main.ts
@@ -17,7 +17,6 @@ const powerService = powerSystem();
 // windows처리 필요
 // Set userData path
 app.setPath('userData', path.join(app.getPath('userData'), RSBUILD_MODE));
-app.setPath('exe', path.join(app.getPath('exe'), RSBUILD_MODE));
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
